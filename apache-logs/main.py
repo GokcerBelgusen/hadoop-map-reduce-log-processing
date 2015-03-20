942ebe8a972547804c88b8dd5a3734c4bfe8f644
Lucian Tuca <tzclucian@gmail.com>
2015-03-20 14:35:20 +0200

    Saving dictionaries in memory for every file and inserting once for each file in the DB.

--- apache-logs/main.py
+++ apache-logs/main.py
@@ -7,40 +7,47 @@ import gzip
 import os
 import redis
 
-from re import findall
 from time import time
 from time import gmtime, strftime
 from threading import Thread
 
 redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
+redis_client.flushall()
+
+input_folder = 'data'
+input_files = []
+data_files = os.listdir(input_folder)
+for input_file in data_files:
+    file_path = os.path.join(input_folder, input_file)
+    input_files.append(file_path)
 
 
 def get_ip(line):
-    for i in range(0, 16):
+    for i in range(7, 16):
         if line[i] == ' ':
             return line[0:i]
 
 
 def get_bytes(line):
-    regexp = '\d{3}\s(\d*)\s'
-    return findall(regexp, line)[0]
+    before_status_space = -1
+    start_space = -1
+    last_space = -1
+    line_length = len(line) - 4
 
-
-def update_in_db(_ip, _bytes):
-    val = redis_client.get(_ip)
-
-    if val is None:
-        val = 0
-
-    val = long(val)
-    redis_client.set(_ip, val + long(_bytes))
-
-
-def process_line(line):
-    _ip = get_ip(line)
-    _bytes = get_bytes(line)
-    # update_in_db(_ip, _bytes)
-    redis_client.lpush(_ip, _bytes)
+    for i in range(0, line_length):
+        if (line[i] == ' ') and (47 < ord(line[i + 1]) < 58) and (47 < ord(line[i + 2]) < 58) \
+                and (47 < ord(line[i + 3]) < 58) and (line[i + 4] == ' '):
+            before_status_space = i + 1
+            break
+    for i in range(before_status_space + 1, line_length):
+        if line[i] == ' ':
+            start_space = i
+            break
+    for i in range(start_space + 1, line_length):
+        if line[i] == ' ':
+            last_space = i
+            break
+    return line[start_space + 1:last_space]
 
 
 def process_zip(zip_path):
@@ -49,11 +56,21 @@ def process_zip(zip_path):
 
     if not redis_client.exists(zip_path):
         print "PROCESSING: %s." % zip_path
+
+        data = {}
+
         process_file = gzip.GzipFile(zip_path, 'rb')
         for line in process_file:
-            process_line(line)
+            _ip = get_ip(line)
+            _bytes = long(get_bytes(line))
+
+            if _ip in data:
+                data[_ip] += _bytes
+            else:
+                data[_ip] = _bytes
         process_file.close()
-        redis_client.set(zip_path, True)
+
+        redis_client.hmset(zip_path, data)
         print "DONE: %s." % zip_path
     else:
         print "ALREADY DONE : %s." % zip_path
@@ -67,23 +84,15 @@ def process_zip(zip_path):
 
 
 def normal():
-    input_folder = 'data'
-    input_files = os.listdir(input_folder)
-
-    for input_file in input_files:
-        file_path = os.path.join(input_folder, input_file)
-        process_zip(file_path)
+    for zip_file in input_files:
+        process_zip(zip_file)
 
 
 def multi_threading():
-    input_folder = 'data'
-    input_files = os.listdir(input_folder)
-
     threads = []
 
-    for input_file in input_files:
-        file_path = os.path.join(input_folder, input_file)
-        threads.append(Thread(target=process_zip, args=(file_path,)))
+    for zip_file in input_files:
+        threads.append(Thread(target=process_zip, args=(zip_file,)))
 
     for thread in threads:
         thread.start()
@@ -92,26 +101,24 @@ def multi_threading():
 
 
 def main():
-    redis_client.flushall()
     start = time()
 
-    #normal()
-    multi_threading()
+    normal()
+    # multi_threading()
 
     end = time()
 
+    ip = sys.argv[1]
+    total_traffic = 0
+
+    for zip_file in input_files:
+        if redis_client.hexists(zip_file, ip):
+            traffic_values = long(redis_client.hget(zip_file, ip))
+            total_traffic += traffic_values
+    print total_traffic
+
     print "TOTAL TIME: %s" % str(end - start)
 
-    ip = sys.argv[1]
-    if redis_client.exists(ip):
-        # traffic = redis_client.get(ip)
-        # print "Traffic for IP %s = %s bytes" % (ip, str(traffic))
-
-        traffic_values = redis_client.lrange(ip, 0, -1)
-        total_traffic = 0
-        for number in traffic_values:
-            total_traffic += long(number)
-        print total_traffic
 
 if __name__ == "__main__":
     main()
