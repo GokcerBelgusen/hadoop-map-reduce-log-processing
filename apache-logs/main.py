a1c9892502582b043d586c6666b76ca4f0191be1
Lucian Tuca <tzclucian@gmail.com>
2015-03-23 10:38:23 +0200

    Some improvements. Saving a dictionary in the db for each file.

--- apache-logs/main.py
+++ apache-logs/main.py
@@ -10,6 +10,7 @@ import redis
 from time import time
 from time import gmtime, strftime
 from threading import Thread
+from itertools import islice
 
 redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
 redis_client.flushall()
@@ -18,8 +19,9 @@ input_folder = 'data'
 input_files = []
 data_files = os.listdir(input_folder)
 for input_file in data_files:
-    file_path = os.path.join(input_folder, input_file)
-    input_files.append(file_path)
+    if '.DS_Store' not in input_file:
+        file_path = os.path.join(input_folder, input_file)
+        input_files.append(file_path)
 
 
 def get_ip(line):
@@ -55,32 +57,36 @@ def process_zip(zip_path):
     start_gmt = gmtime()
 
     if not redis_client.exists(zip_path):
+        print "====================="
         print "PROCESSING: %s." % zip_path
 
         data = {}
 
-        process_file = gzip.GzipFile(zip_path, 'rb')
-        for line in process_file:
-            _ip = get_ip(line)
-            _bytes = long(get_bytes(line))
+        with gzip.open(zip_path) as process_file:
 
-            if _ip in data:
-                data[_ip] += _bytes
-            else:
-                data[_ip] = _bytes
-        process_file.close()
+            # lines = islice(process_file, 0, 40000)
+
+            for i, line in enumerate(process_file):
+                _ip = get_ip(line)
+                _bytes = long(get_bytes(line))
+
+                if _ip in data:
+                    data[_ip] += _bytes
+                else:
+                    data[_ip] = _bytes
 
         redis_client.hmset(zip_path, data)
         print "DONE: %s." % zip_path
+        print "---------------------"
     else:
         print "ALREADY DONE : %s." % zip_path
 
     end_f = time()
-    print os.linesep + "=====================" + os.linesep
+
     print "STARTED @ %s" % strftime("%Y-%m-%d %H:%M:%S", start_gmt)
     print "ENDED @ %s" % strftime("%Y-%m-%d %H:%M:%S", gmtime())
     print "TIME FOR %s : %s" % (zip_path, str(end_f - start_f))
-    print os.linesep + "=====================" + os.linesep
+    print "=====================" + os.linesep
 
 
 def normal():
@@ -109,16 +115,24 @@ def main():
     end = time()
 
     ip = sys.argv[1]
-    total_traffic = 0
+    ip_total_traffic = 0
 
     for zip_file in input_files:
         if redis_client.hexists(zip_file, ip):
             traffic_values = long(redis_client.hget(zip_file, ip))
-            total_traffic += traffic_values
-    print total_traffic
+            ip_total_traffic += traffic_values
 
+    print os.linesep + "====================="
+    print "TRAFIC: %s = %s." % (ip, ip_total_traffic)
     print "TOTAL TIME: %s" % str(end - start)
+    print "=====================" + os.linesep
 
+    total_traffic = 0
+    for zip_file in input_files:
+        ip_dict = redis_client.hgetall(zip_file)
+        for ip in ip_dict:
+            total_traffic += long(ip_dict[ip])
+    print "TOTAL TRAFFIC: ", total_traffic
 
 if __name__ == "__main__":
     main()
